755c4d4aadbf671a74bad13a0ffe8745c2f3d74c
Nick Arora <arora.nick@gmail.com>
2016-05-23 18:11:21 -0700

    create basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   resources :chefs, except: [:new]
   get '/register', to: 'chefs#new'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
 end
